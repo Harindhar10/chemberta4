a2c7ef3350f480a8811c4d693eb49723dc5bde73
Harindhar10 <harindhar99@gmail.com>
2026-02-21 00:02:10 +0530

    print0 to log0

--- chemberta4/__init__.py
+++ chemberta4/__init__.py
@@ -1,3 +1,3 @@
-from .utils import print0, is_main_process, get_rank, set_seed, get_task
+from .utils import log0, is_main_process, get_rank, set_seed, get_task
 from .data import MoleculeNetDataset
 from .callbacks import WandbCallback
